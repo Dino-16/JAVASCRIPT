5a6028be5d16cca56703b08d566811f0e17fdc7d
Dino-16 <ceejaycutie11@gmail.com>
2025-09-10 08:11:33 +0800

    5

---
+++ arithmetic.js
@@ -0,0 +1,19 @@
+// Arithmetic Operators in javascript
+
+let num1 = 5;
+let num2 = 6;
+
+// Addition: adds num1 and num2
+console.log("\n", num1 + num2); // Output: 11
+
+// Subtraction: subtracts num2 from num1
+console.log("\n", num1 - num2); // Output: -1
+
+// Multiplication: multiplies num1 and num2
+console.log("\n", num1 * num2); // Output: 30
+
+// Division: divides num1 by num2
+console.log("\n", num1 / num2); // Output: 0.833...
+
+// Modulus: returns the remainder of num1 divided by num2
+console.log("\n", num1 % num2); // Output: 5
