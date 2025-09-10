e5992dfbf572f44c5e73f8937987f4f2a6681fcd
Dino-16 <ceejaycutie11@gmail.com>
2025-09-10 08:37:10 +0800

    3

---
+++ assignment.-operators.js
@@ -0,0 +1,26 @@
+// Assignment Operators in js
+
+let x = 5;
+// += adds 5 to x (same as x = x + 5)
+x += 5;
+console.log("\n", x); // Output: 10
+
+let y = 3;
+// -= subtracts 1 from y (same as y = y - 1)
+y -= 1;
+console.log("\n", y); // Output: 2
+
+let z = 5;
+// *= multiplies z by 5 (same as z = z * 5)
+z *= 5;
+console.log("\n", z); // Output: 25
+
+let w = 3;
+// /= divides w by 3 (same as w = w / 3)
+w /= 3;
+console.log("\n", w); // Output: 1
+
+let u = 3;
+// %= gives the remainder when u is divided by 2 (same as u = u % 2)
+u %= 2;
+console.log("\n", u); // Output: 1
